62f32e64967ada154abfc10272b8dee5a2f58f36
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-06-10 19:05:17 +0530

    addexpense service

--- ExpenseTracker/src/main/java/com/example/ExpenseTracker/service/ExpenseService.java
+++ ExpenseTracker/src/main/java/com/example/ExpenseTracker/service/ExpenseService.java
@@ -1,4 +1,28 @@
 package com.example.ExpenseTracker.service;
 
+
+import com.example.ExpenseTracker.model.Expense;
+import com.example.ExpenseTracker.repository.ExpenseRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ExpenseService {
+
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository){
+        this.expenseRepository = expenseRepository;
+    }
+
+    public void addExpense(Expense expense){
+        expenseRepository.insert(expense);
+    }
+
+    public void updateExpense(){}
+
+    public void getAllExpenses(){}
+
+    public void getExpenseByName(){}
+
+    public void deleteExpense(){}
 }
