18488ea90fcdf04b9f0bc7f5ed28d4595a5fbc89
ThakshilaJayathilake <e18156@eng.pdn.ac.lk>
2024-06-10 22:15:54 +0530

    Database migration

--- ExpenseTracker/src/main/java/com/example/ExpenseTracker/ExpenseTrackerApplication.java
+++ ExpenseTracker/src/main/java/com/example/ExpenseTracker/ExpenseTrackerApplication.java
@@ -1,9 +1,11 @@
 package com.example.ExpenseTracker;
 
+import com.github.cloudyrock.spring.v5.EnableMongock;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableMongock
 public class ExpenseTrackerApplication {
 
 	public static void main(String[] args) {
